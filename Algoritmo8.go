c11ba2873bc349bc3834deb5a87210008578481b
Felps <127206512+Felipemg05@users.noreply.github.com>
2023-06-16 19:26:02 -0300

    Update Algoritmo8.go

--- Algoritmo8.go
+++ Algoritmo8.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"fmt"
+)
+
+func CalcularContaCompartilhada(gastos map[string]float64) map[string]float64 {
+	totalGastos := 0.0
+
+	for _, valor := range gastos {
+		totalGastos += valor
+	}
+
+	mediaGastos := totalGastos / float64(len(gastos))
+
+	resultado := make(map[string]float64)
+	for nome, valor := range gastos {
+		resultado[nome] = valor - mediaGastos
+	}
+
+	return resultado
+}
+
+func main() {
+	gastos := map[string]float64{
+		"João":   100.0,
+		"Maria":  150.0,
+		"Pedro":  120.0,
+		"Carlos": 80.0,
+	}
+
+	resultado := CalcularContaCompartilhada(gastos)
+
+	for nome, valor := range resultado {
+		fmt.Printf("%s: R$ %.2f\n", nome, valor)
+	}
+}
