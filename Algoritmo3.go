2260d3ce7578fd79a6db13792fa1989fe9261574
Felps <127206512+Felipemg05@users.noreply.github.com>
2023-06-16 19:22:28 -0300

    Update Algoritmo3.go

--- Algoritmo3.go
+++ Algoritmo3.go
@@ -1,1 +1,25 @@
 package main
+
+import "fmt"
+
+func SomaValoresMapa(valores map[string]int) int {
+	soma := 0
+
+	for _, valor := range valores {
+		soma += valor
+	}
+
+	return soma
+}
+
+func main() {
+	valores := map[string]int{
+		"valor1": 10,
+		"valor2": 20,
+		"valor3": 30,
+	}
+
+	soma := SomaValoresMapa(valores)
+
+	fmt.Println(soma)
+}
