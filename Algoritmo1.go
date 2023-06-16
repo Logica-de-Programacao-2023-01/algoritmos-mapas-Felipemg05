7196a0e75b0154915a8eb2abe95dc6442003c8a5
Felps <127206512+Felipemg05@users.noreply.github.com>
2023-06-16 19:21:04 -0300

    Update Algoritmo1.go

--- Algoritmo1.go
+++ Algoritmo1.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func ContarPalavras(texto string) map[string]int {
+	palavras := strings.Fields(texto)
+
+	ocorrencias := make(map[string]int)
+
+	for _, palavra := range palavras {
+		ocorrencias[palavra]++
+	}
+
+	return ocorrencias
+}
+
+func main() {
+	texto := "Olá mundo olá mundo ola mundo"
+
+	ocorrencias := ContarPalavras(texto)
+
+	for palavra, ocorrencia := range ocorrencias {
+		fmt.Printf("%s: %d\n", palavra, ocorrencia)
+	}
+}
