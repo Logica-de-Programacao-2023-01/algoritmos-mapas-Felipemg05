c7acab8c29207150a9efdde4146bda3b5b20b341
Felps <127206512+Felipemg05@users.noreply.github.com>
2023-06-16 19:22:55 -0300

    Update Algoritmo4.go

--- Algoritmo4.go
+++ Algoritmo4.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+func ObterAnagramas(palavras []string) map[string][]string {
+	anagramas := make(map[string][]string)
+
+	for _, palavra := range palavras {
+		chave := ordenarString(palavra)
+		anagramas[chave] = append(anagramas[chave], palavra)
+	}
+
+	return anagramas
+}
+
+func ordenarString(s string) string {
+	letras := strings.Split(s, "")
+	sort.Strings(letras)
+	return strings.Join(letras, "")
+}
+
+func main() {
+	palavras := []string{"amor", "roma", "carro", "arco", "mar", "rota", "macaco"}
+
+	gruposAnagramas := ObterAnagramas(palavras)
+
+	for _, grupo := range gruposAnagramas {
+		fmt.Println(grupo)
+	}
+}
