586293a2ddd1077e16267062088f0b2e8180c2af
Felps <127206512+Felipemg05@users.noreply.github.com>
2023-06-16 19:23:28 -0300

    Update Algoritmo5.go

--- Algoritmo5.go
+++ Algoritmo5.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+)
+
+func ObterFrequenciaCaracteres(s string) map[rune]int {
+	frequencia := make(map[rune]int)
+
+	for _, char := range s {
+		frequencia[char]++
+	}
+
+	return frequencia
+}
+
+func main() {
+	s := "Hello, World!"
+
+	frequencia := ObterFrequenciaCaracteres(s)
+
+	for char, freq := range frequencia {
+		fmt.Printf("Caractere: %c, Frequência: %d\n", char, freq)
+	}
+}
