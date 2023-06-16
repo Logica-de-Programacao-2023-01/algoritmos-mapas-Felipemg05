775113b0646c612b2c4859d9c64c49f8fe3f10e9
Felps <127206512+Felipemg05@users.noreply.github.com>
2023-06-16 19:26:27 -0300

    Update Algoritmo9.go

--- Algoritmo9.go
+++ Algoritmo9.go
@@ -1,1 +1,26 @@
 package main
+
+import "fmt"
+
+func Fibonacci(n int) map[int]int {
+	sequencia := make(map[int]int)
+
+	a, b := 0, 1
+
+	for i := 0; a <= n; i++ {
+		sequencia[i] = a
+		a, b = b, a+b
+	}
+
+	return sequencia
+}
+
+func main() {
+	n := 100
+
+	sequencia := Fibonacci(n)
+
+	for i, num := range sequencia {
+		fmt.Printf("Fibonacci(%d) = %d\n", i, num)
+	}
+}
