19f2ac58b82f3fd55b13deb6fc7ad0830f6ffb9b
Felps <127206512+Felipemg05@users.noreply.github.com>
2023-06-16 19:26:54 -0300

    Update Algoritmo10.go

--- Algoritmo10.go
+++ Algoritmo10.go
@@ -1,1 +1,26 @@
 package main
+
+import "fmt"
+
+func CountPairs(numbers []int) map[[2]int]int {
+	pairs := make(map[[2]int]int)
+
+	for i := 0; i < len(numbers); i++ {
+		for j := i + 1; j < len(numbers); j++ {
+			pair := [2]int{numbers[i], numbers[j]}
+			pairs[pair]++
+		}
+	}
+
+	return pairs
+}
+
+func main() {
+	numbers := []int{1, 2, 3, 4, 5, 2, 3, 4, 4}
+
+	pairFrequencies := CountPairs(numbers)
+
+	for pair, frequency := range pairFrequencies {
+		fmt.Printf("Pair %v - Frequency: %d\n", pair, frequency)
+	}
+}
